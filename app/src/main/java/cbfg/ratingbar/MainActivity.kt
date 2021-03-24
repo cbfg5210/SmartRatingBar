cf2316c8916ccf2d84a6e7fa300e1be5498fd7d8
沉璧浮光 <120276969@qq.com>
2021-03-24 11:57:32 +0800

    app：update demo

--- app/src/main/java/cbfg/ratingbar/MainActivity.kt
+++ app/src/main/java/cbfg/ratingbar/MainActivity.kt
@@ -1,11 +1,28 @@
 package cbfg.ratingbar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val srbHRating = findViewById<SmartRatingBar>(R.id.srbHRating)
+
+        val valueAnimator = ValueAnimator
+            .ofFloat(0.0f, 5.0f, 0f, 5.0f)
+            .setDuration(1600L)
+        valueAnimator.addUpdateListener { animation ->
+            srbHRating.ratingNum = animation.animatedValue as Float
+        }
+        valueAnimator.start()
+
+
+        findViewById<SmartRatingBar>(R.id.srbVRating).setOnRatingBarChangeListener { ratingBar, rating ->
+            Toast.makeText(this, "rating = $rating", Toast.LENGTH_SHORT).show()
+        }
     }
 }
